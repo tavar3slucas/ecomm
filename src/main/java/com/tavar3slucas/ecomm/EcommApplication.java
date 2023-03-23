df230ac06a587fa5e824bb9ee29f23a3aeeb2ce7
Lucas Tavares <t.luuccas@gmail.com>
2023-03-23 10:38:09 -0300

    Operação para inserir dados toda vez que a aplicação subir na tabela categoria

--- src/main/java/com/tavar3slucas/ecomm/EcommApplication.java
+++ src/main/java/com/tavar3slucas/ecomm/EcommApplication.java
@@ -1,13 +1,27 @@
 package com.tavar3slucas.ecomm;
 
+import com.tavar3slucas.ecomm.domain.Categoria;
+import com.tavar3slucas.ecomm.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class EcommApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(EcommApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
