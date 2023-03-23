ed0e7735f0c81c2538e010e5c3f9f3c1572bbf5a
Lucas Tavares <t.luuccas@gmail.com>
2023-03-23 09:01:22 -0300

    Inclusão de dependencias, banco de dados h2 e criação da entity na base local h2

--- src/main/java/com/tavar3slucas/ecomm/domain/Categoria.java
+++ src/main/java/com/tavar3slucas/ecomm/domain/Categoria.java
@@ -1,17 +1,23 @@
 package com.tavar3slucas.ecomm.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1l;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
     public Categoria(){
-
     }
 
     public Categoria(Integer id, String name) {
