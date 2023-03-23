0d42f585abb0c3300504cfd41d4bdc5a1a541d25
Lucas Tavares <t.luuccas@gmail.com>
2023-03-23 08:49:56 -0300

    Implementando classe categoria

--- src/main/java/com/tavar3slucas/ecomm/controller/CategoriaController.java
+++ src/main/java/com/tavar3slucas/ecomm/controller/CategoriaController.java
@@ -1,15 +1,26 @@
 package com.tavar3slucas.ecomm.controller;
 
+import com.tavar3slucas.ecomm.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaController {
 
     @GetMapping
-    public String listar (){
-        return "rest está funcionando";
+    public List<Categoria> listar (){
+        Categoria cat1 = new Categoria(1,"Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
+
 }
---
+++ src/main/java/com/tavar3slucas/ecomm/domain/Categoria.java
@@ -0,0 +1,58 @@
+package com.tavar3slucas.ecomm.domain;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+    private Integer id;
+    private String name;
+
+    public Categoria(){
+
+    }
+
+    public Categoria(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
