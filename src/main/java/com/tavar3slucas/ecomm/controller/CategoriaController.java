3a958d4bcd6cea32e8917a291af08005cf5dd151
Lucas Tavares <t.luuccas@gmail.com>
2023-03-23 08:29:53 -0300

    Testando rest pela primeira vez

---
+++ src/main/java/com/tavar3slucas/ecomm/controller/CategoriaController.java
@@ -0,0 +1,15 @@
+package com.tavar3slucas.ecomm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "categorias")
+public class CategoriaController {
+
+    @GetMapping
+    public String listar (){
+        return "rest está funcionando";
+    }
+}
