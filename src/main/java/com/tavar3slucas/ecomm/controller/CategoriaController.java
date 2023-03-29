667c8fd3adbf9ea66ee34fc30078d9e52136fabd
Lucas Tavares <t.luuccas@gmail.com>
2023-03-29 16:11:35 -0300

    Criação de endpoint para inserir nova categoria @Post

--- src/main/java/com/tavar3slucas/ecomm/controller/CategoriaController.java
+++ src/main/java/com/tavar3slucas/ecomm/controller/CategoriaController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,5 +39,13 @@ public class CategoriaController {
     public ResponseEntity<?> listar (@PathVariable("id") Integer id){
         Categoria categoria = categoriaService.find(id);
         return ResponseEntity.ok().body(categoria) ;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createCategoria(@RequestBody Categoria obj){
+        obj = categoriaService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
